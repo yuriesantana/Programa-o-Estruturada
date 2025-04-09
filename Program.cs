1a31e36a301a13e7c33811b8aede678e3e6df226
Yuri <82000131+yuriesantana@users.noreply.github.com>
2025-04-09 19:24:04 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿int anos, dias, meses, resto;
+Console.WriteLine("digite sua idade me dias:");
+dias = Convert.ToInt32(Console.ReadLine());
+anos = dias / 365;
+resto = dias - (anos * 365);
+meses = resto / 30;
+dias = resto - (meses * 30);
+Console.WriteLine("Sua idade em anos é:" + anos + " em meses:" +meses + " em dias:" +dias);

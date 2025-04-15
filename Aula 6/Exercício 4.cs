b6e646eaaea9f9d5202d9519a80e0cb5ea57b649
Yuri <82000131+yuriesantana@users.noreply.github.com>
2025-04-15 20:14:46 -0300

    Delete Aula 6/Exercício 4.cs

--- Aula 6/Exercício 4.cs
+++
@@ -1,7 +0,0 @@
-﻿int valordolar, valorreais, cotacao;
-Console.WriteLine("Digite o valor em reais:");
-valorreais = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("digite o valor da cotação do dólar:");
-valordolar = Convert.ToInt32(Console.ReadLine());
-cotacao = valorreais / valordolar
-Console.WriteLine("O valor de dolares dessa quantia é:" + cotacao);

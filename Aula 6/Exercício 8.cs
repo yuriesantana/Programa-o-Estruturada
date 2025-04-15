967e14433b44e44e35514e42b3f3376b365ff2ad
Yuri <82000131+yuriesantana@users.noreply.github.com>
2025-04-15 01:07:58 -0300

    Create Exercício 8.cs

---
+++ Aula 6/Exercício 8.cs
@@ -0,0 +1,6 @@
+//Exerício que calcula horas para minutos
+int horas, minutos;
+Console.WriteLine("Digite as horas:");
+horas = Convert.ToInt32(Console.ReadLine());
+minutos = horas * 60;
+Console.WriteLine("As horas convertidas são em minutos:" + minutos);

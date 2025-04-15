9f2e374edc5fc013c4bcd3ba7a38d39ef7ffdc77
Yuri <82000131+yuriesantana@users.noreply.github.com>
2025-04-15 20:11:45 -0300

    Update Exercício 1.cs

--- Aula 6/Exercício 1.cs
+++ Aula 6/Exercício 1.cs
@@ -2,5 +2,6 @@
 Console.WriteLine("Digite seu ano de nascimento:");
 anonascimento = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Digite o ano atual:");
+anoatual = Convert.ToInt32(Console.ReadLine());
 idadeatual = anoatual - anonascimento;
 Console.WriteLine("Sua idade é:" + idadeatual);
